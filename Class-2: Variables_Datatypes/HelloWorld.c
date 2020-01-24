f8119c5b21438101a9b471b9e50a0fa2d7b1c278
Nayan <nayanbehera01@gmail.com>
2020-01-24 20:31:09 +0530

    Add files via upload

---
+++ Class-2: Variables_Datatypes/HelloWorld.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdio.h>
+
+int main()
+{
+
+	printf("Hello world!\n");
+	printf("Hello world!\n");
+	return 0;
+}
---
+++ Class-2: Variables_Datatypes/PrintNumber.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdio.h>
+
+int main()
+{
+
+	printf("%d",32);
+	
+	return 0;
+}
